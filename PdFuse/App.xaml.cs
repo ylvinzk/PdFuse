dc675f3fdb2ab24defcb69dd4fd49bcd7e23c0f4
ylvinzk <ylvinzk@NIHILENAS>
2016-12-06 23:41:48 -0400

    Single instance implemented

--- PdFuse/App.xaml.cs
+++ PdFuse/App.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System.Windows;
-using Microsoft.Shell;
+using Microsoft.Shell.App;
 using System;
 using System.Collections.Generic;
 
@@ -10,26 +10,26 @@ namespace PdFuse
     /// Code provided by Arik Poznanski http://blogs.microsoft.co.il/arik/2010/05/28/wpf-single-instance-application/
     /// for single instance application
     /// </summary>
-    public partial class App : Application//, ISingleInstanceApp
+    public partial class App : Application, ISingleInstanceApp
     {
-        //private const string Unique = "UniqueStringForSingleInstanceApp";
+        private const string Unique = "UniqueStringForSingleInstanceApp";
 
-        //[STAThread]
-        //public static void Main()
-        //{
-        //    if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
-        //    {
-        //        var application = new App();
-        //        application.InitializeComponent();
-        //        application.Run();
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
+            {
+                var application = new App();
+                application.InitializeComponent();
+                application.Run();
 
-        //        SingleInstance<App>.Cleanup();
-        //    }
-        //}
+                SingleInstance<App>.Cleanup();
+            }
+        }
 
-        //public bool SignalExternalCommandLineArgs(IList<string> args)
-        //{
-        //    return true;
-        //}
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            return true;
+        }
     }
 }

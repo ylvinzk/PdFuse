9412e9fd21175a9037055189a439d6002da67ff7
ylvinzk <ylvinperez@outlook.com>
2016-12-08 15:31:28 -0400

    Added 2 extractors

---
+++ PdFuse/Model/CompleteExtractor.cs
@@ -0,0 +1,39 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.exceptions;
+using iTextSharp.text.pdf;
+using System.IO;
+
+namespace PdFuse.Model
+{
+    public class CompleteExtractor : Extractor
+    {
+        public CompleteExtractor(string sourcePath, string resultFolderPath)
+            : base(sourcePath, resultFolderPath) { }
+
+        internal void Extract()
+        {
+            try
+            {
+                pdfReader = new PdfReader(sourcePath);
+
+                for (int page = 1; page <= pdfReader.NumberOfPages; page++)
+                {
+                    document = new Document();
+                    pdfCopy =
+                    new PdfCopy(document, new FileStream(resultFolderPath 
+                    + resultFileName + page + ".pdf", FileMode.Create));
+                    document.Open();
+                    pdfCopy.AddPage(pdfCopy.GetImportedPage(pdfReader, page));
+                    document.Close();
+                }
+
+                pdfReader.Close();
+                statusMessage = "Extraction complete.";
+            }
+            catch (InvalidPdfException)
+            {
+                statusMessage = "Damaged source file.";
+            }
+        }
+    }
+}
---
+++ PdFuse/Model/PartialExtractor.cs
@@ -0,0 +1,72 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.exceptions;
+using iTextSharp.text.pdf;
+using System;
+using System.IO;
+
+namespace PdFuse.Model
+{
+    public class PartialExtractor : Extractor
+    {
+        public PartialExtractor(string sourcePath, string resultFolderPath)
+            : base(sourcePath, resultFolderPath){ }
+
+        internal void Extract(string pageSelection)
+        {
+            int firstPage;
+            int lastPage;
+            string pageName;
+            
+            foreach (string pageRange in pageSelection.Split(','))
+            {
+                if (pageRange.Contains("-"))
+                {
+                    firstPage = int.Parse(pageRange.Split('-')[0]);
+                    lastPage = int.Parse(pageRange.Split('-')[1]);
+                    pageName = firstPage + "to" + lastPage;
+                }
+
+                else
+                {
+                    firstPage = int.Parse(pageRange);
+                    lastPage = firstPage;
+                    pageName = firstPage.ToString();
+                }
+
+                string resultPath = resultFolderPath + resultFileName + pageName + ".pdf";
+
+                try
+                {                    
+                    using (FileStream fileStream = new FileStream(resultPath, FileMode.Create))
+                    {
+                        pdfReader = new PdfReader(sourcePath);
+                        document = new Document();
+                        pdfCopy = new PdfCopy(document, fileStream);
+                        document.Open();
+
+                        for (int page = firstPage; page <= lastPage; page++)
+                            pdfCopy.AddPage(pdfCopy.GetImportedPage(pdfReader, page));
+
+                        document.Close();
+                        pdfReader.Close();
+                    }                    
+                }
+                catch (ArgumentException)
+                {
+                    statusMessage = "Invalid selection. Page number is greater than the document.";
+                    File.Delete(resultPath);
+                    return;
+                }
+                catch (InvalidPdfException)
+                {
+                    statusMessage = "Damaged source file.";
+                    File.Delete(resultPath);
+                    return;
+                }
+            }
+
+            statusMessage = "Extraction complete.";
+        }
+    }
+}
+
